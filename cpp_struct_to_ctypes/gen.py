ec1fcc1ad9303219d57597dfe4bafae05d99a7f0
liangali <liang.a.li@intel.com>
2021-10-18 10:41:25 +0800

    remove unused functions

--- cpp_struct_to_ctypes/gen.py
+++ cpp_struct_to_ctypes/gen.py
@@ -1,11 +1,5 @@
 import os
 
-def check_valid(line):
-    if ';' not in line:
-        return False
-    else:
-        return True
-
 def parse_array_name(name):
     dim = []
     pos = name.find('[')
@@ -64,41 +58,6 @@ def get_type_name(line):
         return None, None, []
     var_name, dim = parse_array_name(var_name.strip())
     return var_type, var_name, dim
-
-def gen_ctypes2():
-    struct_name = 'H264PPS'
-    cpp_struct = []
-    output_list = []
-    output_list.append('from pstruct import *\n')
-    output_list.append('class %s(PStruct):' % struct_name)
-    output_list.append('    _fields_ = [')
-
-    with open('h264_pps.h', 'rt') as f:
-        for line in f:
-            if line.find('//') != -1:
-                line = line[:line.find('//')]
-            if len(line.strip()) == 0 or ('/*' in line and '*/' in line) or ('{' in line) or ('}' in line):
-                continue
-            cpp_struct.append(line.strip('\n').strip())
-
-    with open('h264_pps_new.h', 'wt') as f:
-        f.writelines('\n'.join(cpp_struct))
-
-    for line in cpp_struct:
-        vtype, vname, dim = get_type_name(line)
-        if vtype != None and vname != None:
-            if len(dim) == 0:
-                output_list.append('        ("%s", %s),' % (vname, vtype))
-            else:
-                type_str = vtype
-                for d in dim:
-                    type_str = '(%s)*%d' % (type_str, d)
-                output_list.append('        ("%s", %s),' % (vname, type_str))
-
-    output_list.append('    ]\n')
-
-    with open('dxva_def.py', 'wt') as f:
-        f.writelines('\n'.join(output_list))
 
 def find_struct(header_file):
     with open(header_file, 'rt') as f:
